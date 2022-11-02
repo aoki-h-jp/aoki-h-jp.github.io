db812ae2cb7b5cf6b8350d5a8ae8060cdba53434
aoki-h-jp <aoki.hirotaka.biz@gmail.com>
2022-11-02 20:54:56 +0900

    Add portfolio (playground) #2

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -7,8 +7,14 @@ export default function Portfolio() {
             <div className="container">
                 <div className="portfolio">
                     <h1>Portfolio</h1>
-                    <div className="row">
-                        <p className="text-muted">{t("portfolio.text")}</p>
+                    <br></br>
+                    <div className="card">
+                        <img src="./img/playground.png" className="card-img-top" alt="playground"></img>
+                        <div className="card-body">
+                            <h5 className="card-title"><a href='https://github.com/aoki-h-jp/playground'>playground</a></h5>
+                            <h6 className="card-subtitle mb-2 text-muted">{t("portfolio.cardPlayground.subTitle")}</h6>
+                            <p className="card-text">{t("portfolio.cardPlayground.subscription")}</p>
+                        </div>
                     </div>
                 </div>
             </div>
